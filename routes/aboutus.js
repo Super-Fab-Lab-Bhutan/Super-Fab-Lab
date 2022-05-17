7608ad0e99029c2312140180550fe0d4edb4c239
subham <shubhamchhetri.cse19@chitkarauniversity.edu.in>
2022-05-17 17:14:54 +0600

    added team function and protected routes

--- routes/aboutus.js
+++ routes/aboutus.js
@@ -3,21 +3,22 @@ const router = express.Router();
 
 const { adminAuth } = require("../middleware/auth");
 const { imageUpload } = require("../middleware/file-upload");
-const {getTeam,getAdminTeam,newTeam,teamDelete,postTeam}=require("../controller/aboutus");
+const {
+  getTeam,
+  getAdminTeam,
+  newTeam,
+  teamDelete,
+  postTeam,
+} = require("../controller/aboutus");
 
-router.get("/team",getTeam);
+router.get("/team", adminAuth, getTeam);
 
-router.get("/admin/team",getAdminTeam);
+router.get("/admin/team", adminAuth, getAdminTeam);
 
-router.get("/admin/team/new",newTeam);
+router.get("/admin/team/new", adminAuth, newTeam);
 
-router.post(
-    "/admin/addTeam",
-    imageUpload.single("image"),
-    postTeam
-  );
+router.post("/admin/addTeam", adminAuth, imageUpload.single("image"), postTeam);
 
-router.delete("/admin/team/:id", teamDelete);
-
+router.delete("/admin/team/:id", adminAuth, teamDelete);
 
 module.exports = router;
