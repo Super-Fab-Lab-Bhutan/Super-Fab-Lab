ddaa71b20b0b0683e62c340cc82d38f616968adb
SubhamChhetri <70643794+SubhamChhetri@users.noreply.github.com>
2022-08-23 12:50:20 +0600

    Update booking.js

--- controller/booking.js
+++ controller/booking.js
@@ -284,10 +284,387 @@ exports.equipmentBooking = async (req, res) => {
                 UserOrganization,
               };
               // sheets({ data });
-              const bid = "";
-              const subject = "Your Booking is Confirmed. Booking ID: " + bid;
-              const message = `Equipment: ${EquipmentName}<br>Equipment ID: ${EquipmentType}<br>TIme: ${time}</br>Date: ${date}`;
-              // SendEmail(UserEmail, message, subject);
+             
+              const subject = "Your Booking for ${EquipmentName} is Confrimed;
+              const message = `<head>
+	<style>
+		* {
+			box-sizing: border-box;
+		}
+
+		body {
+			margin: 0;
+			padding: 0;
+		}
+
+		a[x-apple-data-detectors] {
+			color: inherit !important;
+			text-decoration: inherit !important;
+		}
+
+		#MessageViewBody a {
+			color: inherit;
+			text-decoration: none;
+		}
+
+		p {
+			line-height: inherit
+		}
+
+		.desktop_hide,
+		.desktop_hide table {
+			mso-hide: all;
+			display: none;
+			max-height: 0px;
+			overflow: hidden;
+		}
+
+		@media (max-width:660px) {
+			.desktop_hide table.icons-inner {
+				display: inline-block !important;
+			}
+
+			.icons-inner {
+				text-align: center;
+			}
+
+			.icons-inner td {
+				margin: 0 auto;
+			}
+
+			.row-content {
+				width: 100% !important;
+			}
+
+			.mobile_hide {
+				display: none;
+			}
+
+			.stack .column {
+				width: 100%;
+				display: block;
+			}
+
+			.mobile_hide {
+				min-height: 0;
+				max-height: 0;
+				max-width: 0;
+				overflow: hidden;
+				font-size: 0px;
+			}
+
+			.desktop_hide,
+			.desktop_hide table {
+				display: table !important;
+				max-height: none !important;
+			}
+		}
+	</style>
+</head>
+
+<body style="background-color: #f8f8f9; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+	<table border="0" cellpadding="0" cellspacing="0" class="nl-container" role="presentation"
+		style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f8f8f9;" width="100%">
+		<tbody>
+			<tr>
+				<td>
+					<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-1"
+						role="presentation"
+						style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #1aa19c;" width="100%">
+						<tbody>
+							<tr>
+								<td>
+									<table align="center" border="0" cellpadding="0" cellspacing="0"
+										class="row-content stack" role="presentation"
+										style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; background-color: #1aa19c; width: 640px;"
+										width="640">
+										<tbody>
+											<tr>
+												<td class="column column-1"
+													style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 0px; padding-bottom: 0px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"
+													width="100%">
+													<table border="0" cellpadding="0" cellspacing="0"
+														class="divider_block block-1" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+														width="100%">
+														<tbody>
+															<tr>
+																<td class="pad">
+																	<div align="center" class="alignment">
+																		<table border="0" cellpadding="0"
+																			cellspacing="0" role="presentation"
+																			style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+																			width="100%">
+																			<tbody>
+																				<tr>
+																					<td class="divider_inner"
+																						style="font-size: 1px; line-height: 1px; border-top: 4px solid #1AA19C;">
+																						<span>&hairsp;</span></td>
+																				</tr>
+																			</tbody>
+																		</table>
+																	</div>
+																</td>
+															</tr>
+														</tbody>
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-2"
+						role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+						<tbody>
+							<tr>
+								<td>
+									<table align="center" border="0" cellpadding="0" cellspacing="0"
+										class="row-content stack" role="presentation"
+										style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 640px;"
+										width="640">
+										<tbody>
+											<tr>
+												<td class="column column-1"
+													style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"
+													width="100%">
+													<table border="0" cellpadding="0" cellspacing="0"
+														class="image_block block-1" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+														width="100%">
+														<tbody>
+															<tr>
+																<td class="pad"
+																	style="width:100%;padding-right:0px;padding-left:0px;">
+																	<div align="center" class="alignment"
+																		style="line-height:10px"><a
+																			href="www.example.com" style="outline:none"
+																			tabindex="-1" target="_blank"><img
+																				alt="Your logo."
+																				src="https://superfablabbhutan.bt/images/favicon.png"
+																				style="display: block; height: auto; border: 0; width: 160px; max-width: 100%;"
+																				title="Your logo." width="160"></a>
+																	</div>
+																</td>
+															</tr>
+														</tbody>
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-3"
+						role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+						<tbody>
+							<tr>
+								<td>
+									<table align="center" border="0" cellpadding="0" cellspacing="0"
+										class="row-content stack" role="presentation"
+										style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #fff; color: #000000; width: 640px;"
+										width="640">
+										<tbody>
+											<tr>
+												<td class="column column-1"
+													style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 0px; padding-bottom: 0px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"
+													width="100%">
+													<table border="0" cellpadding="0" cellspacing="0"
+														class="divider_block block-1" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+														width="100%">
+														<tbody>
+															<tr>
+
+															</tr>
+														</tbody>
+													</table>
+													<table border="0" cellpadding="0" cellspacing="0"
+														class="text_block block-2" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
+														width="100%">
+														<tbody>
+															<tr>
+																<td class="pad"
+																	style="padding-bottom:10px;padding-left:40px;padding-right:40px;padding-top:10px;">
+																	<div
+																		style="font-family: Georgia, 'Times New Roman', serif">
+																		<div class="txtTinyMce-wrapper"
+																			style="font-size: 12px; font-family: Georgia, Times, 'Times New Roman', serif; mso-line-height-alt: 14.399999999999999px; color: #555555; line-height: 1.2;">
+
+																		</div>
+																	</div>
+																</td>
+															</tr>
+														</tbody>
+													</table>
+													<table border="0" cellpadding="0" cellspacing="0"
+														class="text_block block-3" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
+														width="100%">
+														<tbody>
+															<tr>
+																<td class="pad"
+																	style="padding-bottom:10px;padding-left:40px;padding-right:40px;padding-top:10px;">
+																	<div
+																		style="font-family: Georgia, 'Times New Roman', serif">
+																		<div class="txtTinyMce-wrapper"
+																			style="font-size: 12px; font-family: Georgia, Times, 'Times New Roman', serif; mso-line-height-alt: 18px; color: #555555; line-height: 1.5;">
+																			<p
+																				style="margin: 0; font-size: 18px; text-align: center;">
+																				<strong><span
+																						style="font-size:22px;">Thank
+																						you for booking our
+																						machines.</span></strong> </p>
+																			<p
+																				style="margin: 0; font-size: 18px; text-align: center; mso-line-height-alt: 18px;">
+																				&nbsp;</p>
+																			<p
+																				style="margin: 0; font-size: 18px; text-align: center;">
+																				Your booking has been confirmed
+																				for&nbsp;</p>
+																			<p
+																				style="margin: 0; font-size: 18px; text-align: center; mso-line-height-alt: 18px;">
+																				&nbsp;</p>
+																			<p
+																				style="margin: 0; font-size: 18px; text-align: center;">
+																				Equipment ID:
+																				<strong>${EquipmentId}</strong></p>
+																			<p
+																				style="margin: 0; font-size: 18px; text-align: center; mso-line-height-alt: 27px;">
+																				<span style="font-size:18px;">Equipment
+																					Name:
+																					<strong>${EquipmentName}</strong></span>
+																			</p>
+																			<p
+																				style="margin: 0; font-size: 18px; text-align: center; mso-line-height-alt: 27px;">
+																				Equipment Type:
+																				<strong>${EquipmentType}</strong><br><span
+																					style="font-size:18px;">Time:
+																					<strong>${time}</strong></span></p>
+																			<p
+																				style="margin: 0; font-size: 18px; text-align: center; mso-line-height-alt: 27px;">
+																				<span style="font-size:18px;">Date:
+																					<strong>${date}</strong></span><br><br>
+																			</p>
+																			<p
+																				style="margin: 0; font-size: 18px; text-align: center;">
+																				<em>Please be mindful, you cannot cancel
+																					your booking on the day of your
+																					booking so please contact the lab
+																					for any changes.&nbsp;</em></p>
+																			<p
+																				style="margin: 0; font-size: 18px; text-align: center; mso-line-height-alt: 18px;">
+																				&nbsp;</p>
+																			<p
+																				style="margin: 0; font-size: 18px; text-align: center;">
+																				<em><span style="font-size:17px;">For
+																						further inquires, please contact
+																						our team. <br>We will see you
+																						soon.</span></em></p>
+																			<p
+																				style="margin: 0; font-size: 18px; text-align: center; mso-line-height-alt: 18px;">
+																				&nbsp;</p>
+																		</div>
+																	</div>
+																</td>
+															</tr>
+														</tbody>
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<table align="center" border="0" cellpadding="0" cellspacing="0" class="row row-4"
+						role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+						<tbody>
+							<tr>
+								<td>
+									<table align="center" border="0" cellpadding="0" cellspacing="0"
+										class="row-content stack" role="presentation"
+										style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; background-color: #410125; width: 640px;"
+										width="640">
+										<tbody>
+											<tr>
+												<td class="column column-1"
+													style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 0px; padding-bottom: 0px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;"
+													width="100%">
+													<table border="0" cellpadding="0" cellspacing="0"
+														class="divider_block block-1" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+														width="100%">
+														<tbody>
+															<tr>
+																<td class="pad"
+																	style="padding-bottom:10px;padding-left:40px;padding-right:40px;padding-top:25px;">
+																	<div align="center" class="alignment">
+																		<table border="0" cellpadding="0"
+																			cellspacing="0" role="presentation"
+																			style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;"
+																			width="100%">
+																			<tbody>
+																				<tr>
+																					<td class="divider_inner"
+																						style="font-size: 1px; line-height: 1px; border-top: 1px solid #555961;">
+																						<span>&hairsp;</span></td>
+																				</tr>
+																			</tbody>
+																		</table>
+																	</div>
+																</td>
+															</tr>
+														</tbody>
+													</table>
+													<table border="0" cellpadding="0" cellspacing="0"
+														class="text_block block-2" role="presentation"
+														style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;"
+														width="100%">
+														<tbody>
+															<tr>
+																<td class="pad"
+																	style="padding-bottom:30px;padding-left:40px;padding-right:40px;padding-top:20px;">
+																	<div style="font-family: sans-serif">
+																		<div class="txtTinyMce-wrapper"
+																			style="font-size: 12px; font-family: Montserrat, Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, Tahoma, sans-serif; mso-line-height-alt: 21.6px; color: #555555; line-height: 1.8;">
+																			<p
+																				style="margin: 0; font-size: 14px; text-align: center; mso-line-height-alt: 21.6px; letter-spacing: 1px;">
+																				<span
+																					style="color:#95979c;font-size:12px;">JNW
+																					SuperFabLab<br>Copyright ©
+																					2021</span></p>
+																			<p
+																				style="margin: 0; font-size: 14px; text-align: center; letter-spacing: 1px; mso-line-height-alt: 21.6px;">
+																				&nbsp;</p>
+																		</div>
+																	</div>
+																</td>
+															</tr>
+														</tbody>
+													</table>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+
+				</td>
+			</tr>
+		</tbody>
+	</table><!-- End -->
+
+</body>`;
+             SendEmail(UserEmail, message, subject);
             } catch (e) {
               console.log(e);
             }
